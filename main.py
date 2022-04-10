c4bcd75997509df3bcb2d00085338e0811e6003e
Shahab Bahrami <bahramis@ece.ubc.ca>
2022-04-09 18:12:01 -0700

    Inlude append method for doubly linkedlist class

--- main.py
+++ main.py
@@ -17,5 +17,19 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None :
+            self.tail = new_node
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+            self.length += 1
+        return True
+
 my_DLL= DoublyLinkedList(7)
+my_DLL.append(3)
+my_DLL.append(5)
 my_DLL.print_list()
